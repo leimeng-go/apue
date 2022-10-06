d417e36aea4d35e10cadb77ef41313e3bea1fd95
leimeng-go <ubuntu@localhost.localdomain>
2022-10-06 16:20:23 +0800

    初始化提交

---
+++ README.md
@@ -0,0 +1,12 @@
+## c/c++学习必读
+>[Visual Studio Code 如何编写运行 C、C++ 程序？](https://www.zhihu.com/question/30315894)
+
+>[gcc的基本使用](https://blog.csdn.net/weixin_50697073/article/details/123759516)
+
+>[C语言中文网](http://c.biancheng.net/view/475.html)
+
+### 阅读书籍
+* 《UNIX环境高级编程》
+
+### 学习过程
+1. [GCC使用静态链接库](http://c.biancheng.net/view/7168.html)
---
+++ chapter1/myls.c
@@ -0,0 +1,25 @@
+#include "apue.h"
+#include <dirent.h>
+
+int main(int argc,char *argv[])
+{
+    DIR  *dp;
+    struct dirent *dirp;
+    //限定参数个数
+    if (argc!=2)
+    {
+        err_quit("usage: ls directory_name");
+    }
+    //限定目录值不能为空
+    if ((dp=opendir(argv[1]))==NULL)
+    {
+        err_sys("can't open %s",argv[1]);
+    }
+    //读取
+    while ((dirp=readdir(dp))!=NULL)
+    {
+       printf("%s\n",dirp->d_name);   
+    }
+    closedir(dp);
+    exit(0);
+}
